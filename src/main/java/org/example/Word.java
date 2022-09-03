f61d159ccf0ca05d7b1db51ad80d0f574660b87c
seongyoo <seonlineseon@gmail.com>
2022-09-03 15:03:24 +0900

    creat Word class

---
+++ src/main/java/org/example/Word.java
@@ -0,0 +1,50 @@
+package org.example;
+
+public class Word {
+    int id;
+    int level;
+    String word;
+    String meaning;
+
+    public Word() {
+    }
+
+    public Word(int id, int level, String word, String meaning) {
+        this.id = id;
+        this.level = level;
+        this.word = word;
+        this.meaning = meaning;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+}
