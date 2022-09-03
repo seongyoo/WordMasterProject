0133931fecf931506be78c35b5f325f5e4189726
seongyoo <seonlineseon@gmail.com>
2022-09-03 20:02:11 +0900

    update start

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -1,10 +1,32 @@
 package org.example;
 
-public class WordCRUD implements ICRUD {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class WordCRUD implements ICRUD {
+    ArrayList<Word> list;
+    Scanner s;
+
+    WordCRUD(Scanner s){
+        list = new ArrayList<>();
+        this.s = s;
+    }
     @Override
     public Object add() {
-        return null;
+        System.out.print("=> 난이도(1, 2, 3) & 새 단어 입력 : ");
+        int level = s.nextInt();
+        String word = s.nextLine();
+
+        System.out.print("뜻 입력 : ");
+        String meaning = s.nextLine();
+
+        return new Word(0, level, word, meaning);
+    }
+
+    public  void addWord(){
+        Word one = (Word)add();
+        list.add(one);
+        System.out.println("새 단어가 단어장에 추가되었습니다. ");
     }
 
     @Override
